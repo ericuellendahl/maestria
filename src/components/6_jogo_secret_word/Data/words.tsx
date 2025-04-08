4eaf7de3ce9a2a5abee4cf7014973e6539c8a8c3
eric.uellendahl <eric.uellendahl@gmail.com>
2025-04-08 13:33:19 +0000

    add jogo das palavras

---
+++ src/components/6_jogo_secret_word/Data/words.tsx
@@ -0,0 +1,15 @@
+export const wordList = {
+  dragao: ["Feroz", "Alado", "Fogo", "Lendário", "Gigante"],
+  carro: ["Rápido", "Motorizado", "Rodas", "Transporte", "Eficiente"],
+  fruta: ["Vermelha", "Doce", "Saudável", "Suculenta", "Fresca"],
+  corpo: ["Dedos", "Pé", "Cabeça", "Olhos", "Barriga"],
+  computador: [
+    "Eletrônico",
+    "Processamento",
+    "Memória",
+    "Tecnologia",
+    "Rápido",
+  ],
+  programacao: ["Linguagem", "Web", "Interatividade", "Código", "Lógica"],
+  alimento: ["Redonda", "Queijo", "Massa", "Deliciosa", "Saborosa"],
+};
