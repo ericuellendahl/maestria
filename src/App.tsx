f8acf75c094204a943ad21015f15a324c34ba22c
eric.uellendahl <eric.uellendahl@gmail.com>
2025-04-11 14:43:48 +0000

    add data json e server-json

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import "./App.css";
 
+import { useState, useEffect } from "react";
+
+const url = "https://localhost:3002/products";
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // useEffect(async () => {
+  //   const res = await fetch(url);
+
+  //   const data = await res.json();
+
+  //   setProducts(data);
+
+  // },[]);
+
+  useEffect(() => {
+    fetch('http://localhost:3002/products')
+      .then(response => {
+        console.log('Resposta bruta:', response);
+        return response.json();
+      })
+      .then(data => {
+        console.log('Dados dos produtos:', data);
+        setProducts(data);
+      })
+      .catch(error => console.error('Erro ao buscar produtos:', error));
+  }, []);
+  
+
+  //console.log(products);
+
   return (
     <>
-      <div>
-        <h1>React</h1>
+      <div className="App">
+        <h1>Lista de Produtos</h1>
       </div>
     </>
   );
